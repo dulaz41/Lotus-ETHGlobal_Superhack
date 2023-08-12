697c3e03136e4b07e803c52ae4f82f7a8cda335c
dulaz41 <dulaz41@yahoo.com>
2023-08-12 21:39:54 +0100

    Added index helper script

---
+++ frontend/components/index.ts
@@ -0,0 +1,16 @@
+import Hero from "./Hero";
+import Navbar from  "./Navbar";
+import Footer from './Footer';
+import BarChart from "./BarChart";
+import Dashboard from "./Dashboard";
+import ActiveProposal from "../app/activeproposal/ActiveProposal";
+import Project from "../app/project/Project";
+import Campaign from "../app/campaign/Campaign";
+import SubmitProposal from "./SubmitProposal";
+import Profile from "./Profile";
+import Proposals from "../app/proposal/Proposals";
+import Description from "../app/activeproposal/page";
+
+export {
+    Hero, Navbar, Footer, BarChart, Dashboard, ActiveProposal, Project, Campaign, SubmitProposal, Profile, Proposals, Description
+}
